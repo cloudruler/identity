7c5223e22349642209bf4c484a31f807950a3ed6
Brian Moore <brianmoore2@outlook.com>
2021-03-06 07:36:03 -0600

    New key vault

--- main.tf
+++ main.tf
@@ -18,6 +18,88 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_key_vault" "kv" {
+  name                            = "cloudruler"
+  location                        = azurerm_resource_group.rg.location
+  resource_group_name             = azurerm_resource_group.rg.name
+  sku_name                        = "standard"
+  tenant_id                       = data.azurerm_client_config.current.tenant_id
+  enabled_for_deployment          = true
+  enabled_for_disk_encryption     = true
+  enabled_for_template_deployment = true
+  soft_delete_retention_days      = 90
+  purge_protection_enabled        = true
+
+  access_policy {
+    #Access policy for Azure Infrastructure Automation Service Principal
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    certificate_permissions = [
+      "Backup", "Create", "Delete", "DeleteIssuers", "Get", "GetIssuers", "Import", "List", "ListIssuers",
+      "ManageContacts", "ManageIssuers", "Purge", "Recover", "Restore", "SetIssuers", "Update"
+    ]
+    key_permissions = [
+      "Backup", "Create", "Decrypt", "Delete", "Encrypt", "Get", "Import", "List", "Purge", "Recover",
+      "Restore", "Sign", "UnwrapKey", "Update", "Verify", "WrapKey"
+    ]
+    secret_permissions = [
+      "Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"
+    ]
+    storage_permissions = [
+      "Backup", "Delete", "DeleteSAS", "Get", "GetSAS", "List", "ListSAS",
+      "Purge", "Recover", "RegenerateKey", "Restore", "Set", "SetSAS", "Update"
+    ]
+  }
+
+  # access_policy {
+  #   tenant_id = data.azurerm_client_config.current.tenant_id
+  #   object_id = data.azuread_service_principal.keyvault_admin_spn.id
+  #   certificate_permissions = [
+  #     "Backup", "Create", "Delete", "DeleteIssuers", "Get", "GetIssuers", "Import", "List", "ListIssuers",
+  #     "ManageContacts", "ManageIssuers", "Purge", "Recover", "Restore", "SetIssuers", "Update"
+  #   ]
+  #   key_permissions = [
+  #     "Backup", "Create", "Decrypt", "Delete", "Encrypt", "Get", "Import", "List", "Purge", "Recover",
+  #     "Restore", "Sign", "UnwrapKey", "Update", "Verify", "WrapKey"
+  #   ]
+
+  #   secret_permissions = [
+  #     "Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"
+  #   ]
+
+  #   storage_permissions = [
+  #     "Backup", "Delete", "DeleteSAS", "Get", "GetSAS", "List", "ListSAS",
+  #     "Purge", "Recover", "RegenerateKey", "Restore", "Set", "SetSAS", "Update"
+  #   ]
+  # }
+
+  dynamic "access_policy" {
+    for_each = data.azuread_users.keyvault_admin_users.users
+    iterator = user
+    content {
+      tenant_id = data.azurerm_client_config.current.tenant_id
+      object_id = user.value["object_id"]
+      certificate_permissions = [
+        "Backup", "Create", "Delete", "DeleteIssuers", "Get", "GetIssuers", "Import", "List", "ListIssuers",
+        "ManageContacts", "ManageIssuers", "Purge", "Recover", "Restore", "SetIssuers", "Update"
+      ]
+      key_permissions = [
+        "Backup", "Create", "Decrypt", "Delete", "Encrypt", "Get", "Import", "List", "Purge", "Recover",
+        "Restore", "Sign", "UnwrapKey", "Update", "Verify", "WrapKey"
+      ]
+
+      secret_permissions = [
+        "Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"
+      ]
+
+      storage_permissions = [
+        "Backup", "Delete", "DeleteSAS", "Get", "GetSAS", "List", "ListSAS",
+        "Purge", "Recover", "RegenerateKey", "Restore", "Set", "SetSAS", "Update"
+      ]
+    }
+  }
+}
+
+resource "azurerm_key_vault" "kvnew" {
   name                            = "cloudrulerkvidentity"
   location                        = azurerm_resource_group.rg.location
   resource_group_name             = azurerm_resource_group.rg.name
@@ -30,7 +112,7 @@ resource "azurerm_key_vault" "kv" {
   purge_protection_enabled        = true
 
   access_policy {
-    #Access policy for Terraform Service Principal
+    #Access policy for Azure Infrastructure Automation Service Principal
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
     certificate_permissions = [
