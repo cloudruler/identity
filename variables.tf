c6b17e84fb7642911d908cf682878774c61ea693
Brian Moore <brianmoore2@outlook.com>
2021-03-11 14:29:40 -0600

    Checked in one more file

--- variables.tf
+++ variables.tf
@@ -9,9 +9,3 @@ variable "location" {
 variable "keyvault_admin_users" {
   type = list(string)
 }
-
-variable "generate_keys" {
-  type = bool
-  default = false
-  description = "Use this to regenerate keys. Afterwards, we should remove the state and then run again with this set to false"
-}
