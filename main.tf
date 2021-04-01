38f47056903668015454bc743fb508932a2d7763
Brian Moore <brian.moore@cloudruler.io>
2021-04-01 07:26:14 -0500

    Added new storage account for secure files

--- main.tf
+++ main.tf
@@ -133,3 +133,14 @@ resource "azurerm_app_configuration" "appcs" {
   location                 = var.location
   sku                      = "free"
 }
+
+resource "azurerm_storage_account" "st" {
+  name                     = "cloudruleridentity"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  access_tier              = "Cool"
+  min_tls_version          = "TLS1_2"
+}
